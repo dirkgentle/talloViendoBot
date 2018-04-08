a63bb8574d093c8ae575632b63fae5be739cac8b
DirkGentle <fabian.torres.alvarez@gmail.com>
2018-04-08 19:03:37 -0300

    Added support for curly quotes

--- bot.py
+++ bot.py
@@ -36,13 +36,15 @@ def output_log(text, debug_mode=False): #lo uso para ver el output del bot
     if debug_mode: print(text)
 
 def check_condition(c): #llamaron al bot?
-    aux = PATTERN.search(c.body)
-    if aux == None:
+    #TODO tratar de meter esta primera normalizacion en la regex
+    text = c.body.replace('”','\"').replace('“','\"')
+    match = PATTERN.search(text)
+    if match == None:
         return False
-    elif not aux.group(2) or len(aux.group(2)) < 3:
+    elif not match.group(2) or len(match.group(2)) < 3:
         return 'Montevideo'
     else:
-        return aux.group(2)
+        return match.group(2)
 
 def get_temperature(w):
     temp_dict = w.get_temperature(unit='celsius')
